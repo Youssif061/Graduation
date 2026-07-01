f3264116b0be028597d84897edc9a20b417bb389
Moaz Ashraf <mo3zashraf19@gmail.com>
2026-07-01 17:32:59 +0300

    feat(auth): add change password screen

---
+++ lib/core/constants/app_icon_sizes.dart
@@ -0,0 +1,18 @@
+abstract class AppIconSizes {
+  const AppIconSizes._();
+
+  static const double backArrowWidth = 16;
+  static const double backArrowHeight = 16;
+
+  static const double headerShieldWidth = 16;
+  static const double headerShieldHeight = 20;
+
+  static const double securityLockWidth = 20;
+  static const double securityLockHeight = 20;
+
+  static const double passwordEyeWidth = 22;
+  static const double passwordEyeHeight = 15;
+
+  static const double buttonShieldCheckWidth = 12;
+  static const double buttonShieldCheckHeight = 15;
+}
