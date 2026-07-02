e9696aa5fccfeb35200cd38f7074d3303dc4233c
Youssif Zion <159950680+Youssif061@users.noreply.github.com>
2026-07-03 00:05:36 +0300

    youssif

---
+++ lib/features/SplashScreen/SplashScreen.dart
@@ -0,0 +1,67 @@
+import 'package:expertisemarket/core/constants/app_images.dart';
+import 'package:expertisemarket/core/styles/coloras.dart';
+import 'package:expertisemarket/core/styles/text_styles.dart';
+import 'package:expertisemarket/core/widget/custom_svg_picture.dart';
+import 'package:expertisemarket/core/widget/my%20body.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            AppImages.Splash_Screen,
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          MyBodyView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomSvgPicture(path: AppImages.Background),
+                  Gap(15),
+                  Text(
+                    "ExpertiseMarket",
+                    style: TextStyles.headline.copyWith(
+                      color: AppColors.backgroundColor,
+                    ),
+                  ),
+                  Gap(10),
+                  Text(
+                    "ELITE PROFESSIONALS , EXCEPTIONAL RESULTS",
+                    textAlign: TextAlign.center,
+                    style: TextStyles.subtitle1.copyWith(
+                      color: AppColors.backgroundColor,
+                    ),
+                  ),
+                  Gap(20),
+                  SizedBox(
+                    width: 200,
+                    child: LinearProgressIndicator(
+                      minHeight: 6,
+                      borderRadius: BorderRadius.circular(10),
+                      backgroundColor: Colors.grey.shade300,
+                      valueColor: AlwaysStoppedAnimation(Colors.green),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
