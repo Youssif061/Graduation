8a8b38d7157f8ac0665931801f4f61bab4f906c9
Moaz Ashraf <mo3zashraf19@gmail.com>
2026-07-14 16:18:43 +0300

    feat(user): update bottom navigation icons

--- lib/features/products/presentation/pages/main_shell.dart
+++ lib/features/products/presentation/pages/main_shell.dart
@@ -1,14 +1,20 @@
-import 'package:flutter/material.dart';
+import 'package:expertisemarket/core/constants/app_images.dart';
 import 'package:expertisemarket/core/styles/colors.dart';
-import 'package:expertisemarket/features/home/presentation/pages/home_page.dart';
-import 'package:expertisemarket/features/pros/presentation/pages/pros_page.dart';
-import 'package:expertisemarket/features/products/presentation/pages/products_tab.dart';
-import 'package:expertisemarket/features/request/presentation/pages/request_page.dart';
 import 'package:expertisemarket/features/chats/presentation/pages/chats_page.dart';
+import 'package:expertisemarket/features/home/presentation/pages/home_page.dart';
+import 'package:expertisemarket/features/products/presentation/pages/products_tab.dart';
+import 'package:expertisemarket/features/pros/presentation/pages/pros_page.dart';
+import 'package:expertisemarket/features/request/presentation/pages/request_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainShell extends StatefulWidget {
   final int initialIndex;
-  const MainShell({super.key, this.initialIndex = 2}); // Start on Products tab as requested!
+
+  const MainShell({
+    super.key,
+    this.initialIndex = 2,
+  });
 
   @override
   State<MainShell> createState() => _MainShellState();
@@ -16,12 +22,6 @@ class MainShell extends StatefulWidget {
 
 class _MainShellState extends State<MainShell> {
   late int _currentIndex;
-
-  @override
-  void initState() {
-    super.initState();
-    _currentIndex = widget.initialIndex;
-  }
 
   final List<Widget> _tabs = const [
     HomePage(),
@@ -32,6 +32,12 @@ class _MainShellState extends State<MainShell> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.initialIndex;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.marketBg,
@@ -39,63 +45,57 @@ class _MainShellState extends State<MainShell> {
         index: _currentIndex,
         children: _tabs,
       ),
-      bottomNavigationBar: _buildBottomNav(),
+      bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
-  Widget _buildBottomNav() {
+  Widget _buildBottomNavigationBar() {
     return Container(
       decoration: const BoxDecoration(
         color: AppColors.navBg,
         border: Border(
-          top: BorderSide(color: AppColors.marketBorder, width: 1),
+          top: BorderSide(
+            color: AppColors.marketBorder,
+            width: 1,
+          ),
         ),
       ),
       child: SafeArea(
+        top: false,
         child: SizedBox(
           height: 64,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              _NavItem(
-                index: 0,
-                currentIndex: _currentIndex,
-                icon: Icons.home_outlined,
-                activeIcon: Icons.home,
+              _UserNavItem(
+                iconPath: AppImages.homeUserSvg,
                 label: 'Home',
-                onTap: () => setState(() => _currentIndex = 0),
+                isActive: _currentIndex == 0,
+                onTap: () => _selectTab(0),
               ),
-              _NavItem(
-                index: 1,
-                currentIndex: _currentIndex,
-                icon: Icons.work_outline,
-                activeIcon: Icons.work,
+              _UserNavItem(
+                iconPath: AppImages.prosUserSvg,
                 label: 'Pros',
-                onTap: () => setState(() => _currentIndex = 1),
+                isActive: _currentIndex == 1,
+                onTap: () => _selectTab(1),
               ),
-              _NavItem(
-                index: 2,
-                currentIndex: _currentIndex,
-                icon: Icons.shopping_bag_outlined,
-                activeIcon: Icons.shopping_bag,
+              _UserNavItem(
+                iconPath: AppImages.productsUserSvg,
                 label: 'Products',
-                onTap: () => setState(() => _currentIndex = 2),
+                isActive: _currentIndex == 2,
+                onTap: () => _selectTab(2),
               ),
-              _NavItem(
-                index: 3,
-                currentIndex: _currentIndex,
-                icon: Icons.add_circle_outline,
-                activeIcon: Icons.add_circle,
+              _UserNavItem(
+                iconPath: AppImages.requestUserSvg,
                 label: 'Request',
-                onTap: () => setState(() => _currentIndex = 3),
+                isActive: _currentIndex == 3,
+                onTap: () => _selectTab(3),
               ),
-              _NavItem(
-                index: 4,
-                currentIndex: _currentIndex,
-                icon: Icons.chat_bubble_outline,
-                activeIcon: Icons.chat_bubble,
+              _UserNavItem(
+                iconPath: AppImages.chatsUserSvg,
                 label: 'Chats',
-                onTap: () => setState(() => _currentIndex = 4),
+                isActive: _currentIndex == 4,
+                onTap: () => _selectTab(4),
               ),
             ],
           ),
@@ -103,53 +103,66 @@ class _MainShellState extends State<MainShell> {
       ),
     );
   }
+
+  void _selectTab(int index) {
+    if (_currentIndex == index) {
+      return;
+    }
+
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 }
 
-class _NavItem extends StatelessWidget {
-  final int index;
-  final int currentIndex;
-  final IconData icon;
-  final IconData activeIcon;
+class _UserNavItem extends StatelessWidget {
+  final String iconPath;
   final String label;
+  final bool isActive;
   final VoidCallback onTap;
 
-  const _NavItem({
-    required this.index,
-    required this.currentIndex,
-    required this.icon,
-    required this.activeIcon,
+  const _UserNavItem({
+    required this.iconPath,
     required this.label,
+    required this.isActive,
     required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    final bool isActive = index == currentIndex;
+    final Color itemColor =
+    isActive ? AppColors.navActive : AppColors.navInactive;
 
-    return GestureDetector(
-      onTap: onTap,
-      behavior: HitTestBehavior.opaque,
-      child: SizedBox(
-        width: 64,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              isActive ? activeIcon : icon,
-              color: isActive ? AppColors.navActive : AppColors.navInactive,
-              size: 24,
-            ),
-            const SizedBox(height: 4),
-            Text(
-              label,
-              style: TextStyle(
-                fontSize: 10,
-                fontWeight: isActive ? FontWeight.w700 : FontWeight.w500,
-                color: isActive ? AppColors.navActive : AppColors.navInactive,
-                fontFamily: 'Inter',
+    return Expanded(
+      child: InkWell(
+        onTap: onTap,
+        child: SizedBox(
+          height: 64,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                iconPath,
+                width: 24,
+                height: 24,
+                colorFilter: ColorFilter.mode(
+                  itemColor,
+                  BlendMode.srcIn,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 4),
+              Text(
+                label,
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight:
+                  isActive ? FontWeight.w700 : FontWeight.w500,
+                  color: itemColor,
+                  fontFamily: 'Inter',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
